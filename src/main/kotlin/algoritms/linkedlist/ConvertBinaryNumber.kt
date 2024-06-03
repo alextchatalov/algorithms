c08adf4127b309906a578b5d8c824cb7c99d5f28
Alex Travagin Chatalov <alex.chatalov@ifood.com.br>
2024-06-03 20:50:45 -0300

    convert binary number in a linked list in a number

---
+++ src/main/kotlin/algoritms/linkedlist/ConvertBinaryNumber.kt
@@ -0,0 +1,40 @@
+package algoritms.linkedlist
+
+// Leetcode: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+
+fun main() {
+    getDecimalValue(
+        ListNode(1).apply {
+            next =
+                ListNode(0).apply {
+                    next = ListNode(1)
+                }
+        },
+    ).also {
+        println(it) // 5
+    }
+}
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun getDecimalValue(head: ListNode?): Int {
+    var result = ""
+    var current = head
+    while (current != null) {
+        result += current.`val`
+        current = current.next
+    }
+    return result.toInt(2)
+}
