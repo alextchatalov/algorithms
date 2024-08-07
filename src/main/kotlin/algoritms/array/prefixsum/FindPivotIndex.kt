187e052c9cd6b7575481f2e3acde9df8805633fc
Alex Travagin Chatalov <alex.chatalov@ifood.com.br>
2024-08-07 08:26:04 -0300

    find privot index problem solved

--- src/main/kotlin/algoritms/array/prefixsum/FindPivotIndex.kt
+++ src/main/kotlin/algoritms/array/prefixsum/FindPivotIndex.kt
@@ -1,19 +1,26 @@
 package algoritms.array.prefixsum
-// Leetcode 1732. Find the highest altitude: https://leetcode.com/problems/find-the-highest-altitude/?envType=study-plan-v2&envId=leetcode-75
+// Leetcode 724. Find pivot index: https://leetcode.com/problems/find-pivot-index/?envType=study-plan-v2&envId=leetcode-75
 
 fun main() {
-    val result = largestAltitude(intArrayOf(-5, 1, 5, 0, -7))
+    val result = pivotIndex(intArrayOf(1, 7, 3, 6, 5, 6))
     println(result)
 }
 
 // Time complexity: O(n)
-// Space complexity: O(1)
-fun largestAltitude(gain: IntArray): Int {
-    var max = 0
-    var current = 0
-    for (element in gain) {
-        current += element
-        max = maxOf(max, current)
+// Space complexity: O(n)
+fun pivotIndex(nums: IntArray): Int {
+    val sumLeft: MutableList<Int> = mutableListOf()
+    val sumRight: MutableList<Int> = mutableListOf()
+    for (i in nums.indices) {
+        sumLeft.add(nums[i] + if (i > 0) sumLeft[i - 1] else 0)
     }
-    return max
+    for (i in nums.size - 1 downTo 0) {
+        sumRight.add(nums[i] + if (i < nums.size - 1) sumRight.last() else 0)
+    }
+    for (i in nums.indices) {
+        if (sumLeft[i] == sumRight[nums.size - i - 1]) {
+            return i
+        }
+    }
+    return -1
 }
